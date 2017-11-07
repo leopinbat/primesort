5c63de944fbf224ceb7b54d90b1b26cf08ca911d
leopinbat <leopinbat>
2017-11-07 00:32:31 -0200

    Final_Program

--- src/main.c
+++ src/main.c
@@ -6,12 +6,43 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+ 
+
+int Test_Primo(int n){
+    int primo = 0;
+    
+    for(int k=1; k<=n; k++)
+        if((n%k)==0)
+            primo++;
+    if ((n == 1) || (primo>2) )
+        return 0;
+
+    else
+        return 1;
+
+
+}
 
 int main() {
 
-  int x, y;
+    int n;
+    FILE *file;	
+    file = popen("sort -n -r", "w");
+    while (1){
+	scanf ("%d", &n);
 
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
-  return 0;
+// Criterio de parada em -1
+	if (n >= 0) {
+	    if ((Test_Primo(n) == 0) && (n!=0)){ 			
+	        fprintf (file,"%d\n",n);
+	    }	  			  				  				  			
+	}  	
+	else { 		
+	    pclose(file);
+	    break;	  
+	}
+   }
+
+    return 0;
 }
